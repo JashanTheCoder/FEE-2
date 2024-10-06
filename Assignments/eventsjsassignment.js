df8b7b7aaab02a377eb12fa6b02daeb39e23c269
Jashan <jashan72464@gmail.com>
2024-10-07 00:03:28 +0530

    new work

---
+++ Assignments/eventsjsassignment.js
@@ -0,0 +1,16 @@
+const profileName = document.getElementById('profile-name');
+const profileBio = document.getElementById('profile-bio');
+const profilePic = document.getElementById('profile-pic');
+
+const nameInput = document.getElementById('name-input');
+const bioInput = document.getElementById('bio-input');
+const picInput = document.getElementById('pic-input');
+const updateButton = document.getElementById('update-button');
+
+updateButton.addEventListener('click', function () {
+	profileName.innerText = `Name: ${nameInput.value}`;
+
+	profileBio.innerText = `Bio: ${bioInput.value}`;
+
+	profilePic.src = picInput.value;
+});
